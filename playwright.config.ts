6a60e6f74a0dab06089b5d4a86d4804ced9172a7
shammikadahanayaka <ddshammi@gmail.com>
2025-10-19 19:39:59 -0700

    Fixed Ortoni report config - 39

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
 
   projects: [
     { name: "chromium", use: { ...devices["Desktop Chrome"] } },
-    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
-    { name: "webkit", use: { ...devices["Desktop Safari"] } },
+    //{ name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    //{ name: "webkit", use: { ...devices["Desktop Safari"] } },
   ],
 });
